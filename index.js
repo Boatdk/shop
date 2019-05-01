75782ed8cf039d3dea007aea8d09e0d109abfff6
boatdk <boatz.dk@gmail.com>
2019-05-01 18:24:46 +0700

    v1.4

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public'));
-app.listen(80);
+app.use(express.static(__dirname + '/picture'));
+app.listen(4000);
 console.log("server ready!")
